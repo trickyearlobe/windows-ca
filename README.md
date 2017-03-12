a70b2ac870cf7dbfb12c2c75fe3b6e53ad9e2bc5
Richard Nixon <richard.nixon@btinternet.com>
2017-03-12 22:42:12 +0000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# windows-ca
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,19 @@
+name 'windows-ca'
+maintainer 'Richard Nixon'
+maintainer_email 'richard.nixon@btinternet.com'
+license 'apachev2'
+description 'Installs/Configures windows-ca'
+long_description 'Installs/Configures windows-ca'
+version '0.1.0'
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/windows-ca/issues' if respond_to?(:issues_url)
+
+# The `source_url` points to the development reposiory for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/windows-ca' if respond_to?(:source_url)
